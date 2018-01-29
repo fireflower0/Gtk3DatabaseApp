61f07e935a44fdafbe6ab07530c6e488d8a074dd
fireflower0 <fireflower0@hotmail.co.jp>
2018-01-29 22:45:01 +0900

    fixed window form

--- gtk3-database.lisp
+++ gtk3-database.lisp
@@ -74,20 +74,76 @@
                    (set-data model data-list))))
     view))
 
-(defun example-simple-tree-view ()
+(defun main ()
   (within-main-loop
     (let ((window (make-instance 'gtk-window
                                  :title "Database Viewer"
                                  :type :toplevel
-                                 :border-width 12
-                                 :default-width 300
-                                 :default-height 200))
-          (view (exe-query (db-connect "test" "nobody" "1234") "SELECT * FROM hello")))
+                                 :border-width 12))
+          (db-name-label   (make-instance 'gtk-label :label "DB Name:"))
+          (user-name-label (make-instance 'gtk-label :label "User Name:"))
+          (password-label  (make-instance 'gtk-label :label "Password:"))
+          (db-name-entry   (make-instance 'gtk-entry))
+          (user-name-entry (make-instance 'gtk-entry))
+          (password-entry  (make-instance 'gtk-entry))
+          (connect-button  (make-instance 'gtk-button :label "Connection"))
+          (sql-label       (make-instance 'gtk-label  :label "SQL:"))
+          (sql-entry       (make-instance 'gtk-entry))
+          (query-button    (make-instance 'gtk-button :label "Query"))
+          (view            (make-instance 'gtk-tree-view))
+          (table (make-instance 'gtk-table :n-rows 7 :n-columns 4))
+          (connection nil))
+      ;; Define the signal handlers
       (g-signal-connect window "destroy"
                         (lambda (widget)
                           (declare (ignore widget))
                           (leave-gtk-main)))
-      (gtk-container-add window view)
+      (g-signal-connect connect-button "clicked"
+                        (lambda (widget)
+                          (declare (ignore widget))
+                          (setf connection (db-connect (gtk-entry-text db-name-entry)
+                                                       (gtk-entry-text user-name-entry)
+                                                       (gtk-entry-text password-entry)))
+                          (setf view (exe-query connection "SELECT relname as TABLE_NAME FROM pg_stat_user_tables"))
+                          (gtk-table-attach table view 0 7 3 4)
+                          (gtk-widget-show-all window)))
+      (g-signal-connect query-button "clicked"
+                        (lambda (widget)
+                          (declare (ignore widget))
+                          (if (null connection)
+                              (format t "No connection.")
+                              (progn
+                                (setf view (exe-query connection (gtk-entry-text sql-entry)))
+                                (gtk-table-attach table view 0 7 3 4)
+                                (gtk-widget-show-all window)))))
+      ;; Put the widgets into the table
+      ;;                Row
+      ;;     0   1   2   3   4   5   6   7
+      ;;   0 +---+---+---+---+---+---+---+
+      ;;     |   |   |   |   |   |   |   |
+      ;;   1 +---+---+---+---+---+---+---+
+      ;;     |   |   |   |   |   |   |   |
+      ;; C 2 +---+---+---+---+---+---+---+
+      ;; o   |   |   |   |   |   |   |   |
+      ;; l 3 +---+---+---+---+---+---+---+
+      ;;     |   |   |   |   |   |   |   |
+      ;;   4 +---+---+---+---+---+---+---+
+      ;;                                      R1 R2 C1 C2
+      (gtk-table-attach table db-name-label    0  1  0  1)
+      (gtk-table-attach table user-name-label  2  3  0  1)
+      (gtk-table-attach table password-label   4  5  0  1)
+      (gtk-table-attach table db-name-entry    1  2  0  1)
+      (gtk-table-attach table user-name-entry  3  4  0  1)
+      (gtk-table-attach table password-entry   5  6  0  1)
+      (gtk-table-attach table connect-button   6  7  0  1)
+      (gtk-table-attach table sql-label        0  1  2  3)
+      (gtk-table-attach table sql-entry        1  6  2  3)
+      (gtk-table-attach table query-button     6  7  2  3)
+      (gtk-table-attach table view             0  7  3  4)
+      ;; Put the table into the window
+      (gtk-container-add window table)
+      ;; Show the window
       (gtk-widget-show-all window))))
 
-(example-simple-tree-view)
+;; Execution!
+(main)
